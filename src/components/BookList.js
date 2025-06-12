636eb4274fb3f452b06634d849f5f003ace73100
Adrian Hurt <hurt.adrian@seznam.cz>
2025-06-12 20:19:30 +0200

    title and favicon change

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,8 +5,8 @@ import "./BookList.css";
 const BookList = ({ books }) => {
   return (
     <div className="book-list">
-      {books.map((book, index) => (
-        <BookCard key={index} book={book} />
+      {books.map((book) => (
+        <BookCard key={book.key} book={book} />
       ))}
     </div>
   );
